0519e099e31e7a700788d7292aafb93d97376e05
mechAneko12 <naokih1998@yahoo.co.jp>
2020-10-29 18:04:31 +0900

    Oct 29, 2020

---
+++ download_img.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget "https://drive.google.com/uc?export=download&id=1if-e5sZGlmydNwEuuvOPN6UB9Z_RlexN" -O project1/project1-a/geo_wave.jpg.zip
+unzip -d project1/project1-a/original_img project1/project1-a/geo_wave.jpg.zip
+rm project1/project1-a/geo_wave.jpg.zip
