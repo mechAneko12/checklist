b247d3406bf0857a2ad20bb3161085b62e27c22f
mechAneko12 <naokih1998@yahoo.co.jp>
2020-11-01 22:51:05 +0900

    Nov 1, 2020

--- question2.py
+++ question2.py
@@ -8,11 +8,12 @@ X = iris_dataset['data']
 y = iris_dataset['target']
 
 
-X_new = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
+X_new = np.concatenate((np.ones((X.shape[0], 1)), X), axis=1)
+
 
 def f(x, b):
     return 1.0 / (1.0 + np.exp(-np.dot(x, b.T)))
 
 
 if __name__ == '__main__':
-    print(f(X_new, np.random.normal(0,1,(1, X_new.shape[1]))))
+    print(f(X_new, np.random.normal(0, 1, (1, X_new.shape[1]))))
